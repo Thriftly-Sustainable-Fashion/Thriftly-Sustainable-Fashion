66f2d6a34d8f0c2424ad4914a04fead5b6dddac9
Dandi Gustiawan <125660099+dandi-gus@users.noreply.github.com>
2024-12-10 00:29:12 +0700

    Update index.js

--- thriftly-backend/index.js
+++ thriftly-backend/index.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 
 const app = express();  
 const PORT = process.env.PORT || 3000;  
+app.listen(PORT, '0.0.0.0', () => {  
+    console.log(`Server running on port ${PORT}`);  
+});  
 
 // Middleware  
 app.use(cors());  
